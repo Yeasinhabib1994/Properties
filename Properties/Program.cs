e469d1b2a3ed294aa5f3ad9c68f1485f0a5e7e9a
yeasin habib <yeasinhabib1994@gmail.com>
2018-10-08 18:18:37 +0600

    Add project files.

---
+++ Properties/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var person = new Person();
+            //person.Birthdate = new DateTime(1994, 4, 1);
+
+            var person = new Person(new DateTime(1994, 4, 1));
+            Console.WriteLine("Birthdate: " + person.Birthdate);
+            Console.WriteLine("Age: " + person.Age);
+        }
+    }
+
+    public class Person
+    {
+        //public DateTime Birthdate { get; set; }
+
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+
+        }
+    }
+}
